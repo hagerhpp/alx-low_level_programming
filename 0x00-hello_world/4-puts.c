94ec396b04b12c77daba9b8302249e5b62f552ec
hagerhpp <loloahmeda18@gmail.com>
2023-07-13 17:53:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 /**
  * it - print using puts
  *
- * This function prints a string using the puts function.
- *
+ * Description:A C this function prints a string using the puts function
+ * .
+ * main - it print output using put
  * Return: Always 0 (success)
  */
 int main(void)
