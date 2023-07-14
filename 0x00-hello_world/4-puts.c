a14eec7b7144c7e648ffaecca164a5f75e5dcc8b
hagerhpp <loloahmeda18@gmail.com>
2023-07-13 17:27:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * it return (0)
  */
-int main(void) 
+int main (void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
-         return (0);
+      puts ("\"Programming is like building a multilingual puzz");
+       return  (0);
 }
